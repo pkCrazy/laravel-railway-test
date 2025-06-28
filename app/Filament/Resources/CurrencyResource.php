0f06de1a64b9c8af26d15e2c010392c7d1827d99
Tamás Kis <kis6724@gmail.com>
2025-06-28 13:39:03 +0200

    install filamentphp and create a currency resource

---
+++ app/Filament/Resources/CurrencyResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CurrencyResource\Pages;
+use App\Filament\Resources\CurrencyResource\RelationManagers;
+use App\Models\Currency;
+use BcMath\Number;
+use Dom\Text;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CurrencyResource extends Resource
+{
+    protected static ?string $model = Currency::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Currency Name')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+                TextInput::make('code')
+                    ->label('Currency Code')
+                    ->required()
+                    ->maxLength(10)
+                    ->unique(ignoreRecord: true),
+                TextInput::make('exchange_rate')
+                    ->label('Exchange Rate')
+                    ->numeric()
+                    ->default(1.0000)
+                    ->step(0.01)
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Currency Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('code')
+                    ->label('Currency Code')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('exchange_rate')
+                    ->label('Exchange Rate')
+                    ->numeric(decimalPlaces: 4)
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCurrencies::route('/'),
+            'create' => Pages\CreateCurrency::route('/create'),
+            'edit' => Pages\EditCurrency::route('/{record}/edit'),
+        ];
+    }
+}
